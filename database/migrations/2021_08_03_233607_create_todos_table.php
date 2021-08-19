543395502d12c1e3ac524be77a5b8d668af83ff3
鈴木翔太 <suzukishouta@suzukishoutanoMacBook-Pro.local>
2021-08-19 12:00:07 +0900

    sixth

--- database/migrations/2021_08_03_233607_create_todos_table.php
+++ database/migrations/2021_08_03_233607_create_todos_table.php
@@ -14,7 +14,7 @@ class CreateTodosTable extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-            $table->bigInteger('id');
+            $table->id();
             $table->string('content',191);
             $table->timestamp('created_at')->useCurrent()->nullable();
             $table->timestamp('updated_at')->useCurrent()->nullable();
